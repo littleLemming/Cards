75794dc2db3fd6cb488d2ef76d1a441f6c56802f
littleLemming <lemming0061@gmail.com>
2015-08-02 15:57:55 +0200

    let's now change everything. =D

--- Kartenstapel.h
+++ Kartenstapel.h
@@ -2,21 +2,22 @@
 #define KARTENSTAPEL_H
 
 #include <vector>
-#include <stack>
+#include <list>
 
 #include "Karte.h"
 #include "Ablagestapel.h"
 
 class Kartenstapel {
 private:
-	std::stack<Karte> karten;
+	std::list<Karte> karten;
 	std::vector<Karte> makeVector(void);
-	void makeStack(std::vector<Karte>);
+	void makeList(std::vector<Karte>);
 public:
 	Kartenstapel();
 	~Kartenstapel();
-	void toString(void);
+	std::string toString(void);
 	Karte takeCard(void);
+	// TODO:
 	void mergeWithAblagestapel(Ablagestapel);
 	void mischen(void);
 };
